f98d198a35973dd843ab5ebd9bcdbfe40561ad70
Satyam <68682757+satyam2337@users.noreply.github.com>
2024-11-29 16:29:34 +0530

    Update script.js
    Key update

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ async function generateImage() {
       "https://api-inference.huggingface.co/models/black-forest-labs/FLUX.1-dev",
       {
           headers: {
-              Authorization: "Bearer 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",
+              Authorization: "Bearer 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",
               "Content-Type": "application/json",
           },
           method: "POST",
